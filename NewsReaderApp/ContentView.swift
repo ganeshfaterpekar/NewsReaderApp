58631922014c5d17c97fb8f62758a59b0f3b8493
Ganesh Faterpekar <ganeshfaterpekar@gmail.com>
2026-01-01 22:19:12 +1100

    Initial Commit

---
+++ NewsReaderApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsReaderApp
+//
+//  Created by Ganesh Faterpekar on 1/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsReaderApp/NewsReaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderAppApp.swift
+//  NewsReaderApp
+//
+//  Created by Ganesh Faterpekar on 1/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
